ea51f5aeae64c643fdf2933b667fa2ba344f2175
Ruben <ruben.jaramillo@istvidanueva.edu.ec>
2025-11-08 11:31:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,65 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.calculadora.ui.theme.CalculadoraTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        //Referencias a los elementos de UX
+        val txtnum1 = findViewById<EditText>(R.id.txtNumero1)
+        val txtnum2 = findViewById<EditText>(R.id.txtNumero2)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+        val btnSumar = findViewById<Button>(R.id.btnSumar)
+        val btnRestar = findViewById<Button>(R.id.btnRestar)
+        val btnMultiplicar = findViewById<Button>(R.id.btnMultiplicar)
+        val btnDividir = findViewById<Button>(R.id.btnDividir)
+        //funciones auxiliares para valiudar la entrada
+        fun obtenerNumeros(): Pair<Double?, Double?> {
+            val num1 = txtnum1.text.toString()
+            val num2 = txtnum2.text.toString()
+            if(num1.isEmpty() || num2.isEmpty()) {
+             Toast.makeText(this, "Ingrese ambos números", Toast.LENGTH_SHORT).show()
+                return Pair(null, null)
+            }
+            return Pair(num1.toDoubleOrNull(), num2.toDoubleOrNull())
+        }
+    //eventos: sumar
+        btnSumar.setOnClickListener {
+            val (a, b) = obtenerNumeros()
+            if (a != null && b != null) {
+                txtResultado.text = "Resultado: ${a + b}"
+            }
+        }
+        //eventos: restar
+        btnRestar.setOnClickListener {
+            val (a, b) = obtenerNumeros()
+            if (a != null && b != null) {
+                txtResultado.text = "Resultado: ${a - b}"
+            }
+        }
+        //evento multiplicar
+        btnMultiplicar.setOnClickListener {
+            val (a, b) = obtenerNumeros()
+            if (a != null && b != null) {
+                txtResultado.text = "Resultado: ${a * b}"
+            }
+        }
+    }
+}
